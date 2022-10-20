132aa9cffd59a6d3459403430ecbb8333c8e8fb3
Jorge Herrera <jrgherrera@gmail.com>
2022-10-19 19:47:51 -0600

    Increase magazine spawn rate, create VHS, add metal weapon

--- Contents/mods/Potato Launcher/media/lua/server/POTATO_ProceduralDistributions.lua
+++ Contents/mods/Potato Launcher/media/lua/server/POTATO_ProceduralDistributions.lua
@@ -17,7 +17,7 @@ table.insert(ProceduralDistributions.list["ArtSupplies"].items, "PotatoLauncher.
 table.insert(ProceduralDistributions.list["ArtSupplies"].items, 4);
 
 table.insert(ProceduralDistributions.list["BookstoreMisc"].items, "PotatoLauncher.POTATO_PotatoLauncherMag");
-table.insert(ProceduralDistributions.list["BookstoreMisc"].items, 2);
+table.insert(ProceduralDistributions.list["BookstoreMisc"].items, 4);
 
 table.insert(ProceduralDistributions.list["BurglarTools"].items, "PotatoLauncher.POTATO_SprayPaint");
 table.insert(ProceduralDistributions.list["BurglarTools"].items, 4);
@@ -28,12 +28,12 @@ table.insert(ProceduralDistributions.list["ClassroomDesk"].items, 4);
 table.insert(ProceduralDistributions.list["ClassroomMisc"].items, "PotatoLauncher.POTATO_SprayPaint");
 table.insert(ProceduralDistributions.list["ClassroomMisc"].items, 4);
 table.insert(ProceduralDistributions.list["ClassroomMisc"].items, "PotatoLauncher.POTATO_PotatoLauncherMag");
-table.insert(ProceduralDistributions.list["ClassroomMisc"].items, 1);
+table.insert(ProceduralDistributions.list["ClassroomMisc"].items, 3);
 
 table.insert(ProceduralDistributions.list["ClassroomShelves"].items, "PotatoLauncher.POTATO_SprayPaint");
 table.insert(ProceduralDistributions.list["ClassroomShelves"].items, 4);
 table.insert(ProceduralDistributions.list["ClassroomShelves"].items, "PotatoLauncher.POTATO_PotatoLauncherMag");
-table.insert(ProceduralDistributions.list["ClassroomShelves"].items, 1);
+table.insert(ProceduralDistributions.list["ClassroomShelves"].items, 3);
 
 table.insert(ProceduralDistributions.list["ClosetShelfGeneric"].items, "PotatoLauncher.POTATO_SprayPaint");
 table.insert(ProceduralDistributions.list["ClosetShelfGeneric"].items, 4);
@@ -41,16 +41,16 @@ table.insert(ProceduralDistributions.list["ClosetShelfGeneric"].items, "Base.Woo
 table.insert(ProceduralDistributions.list["ClosetShelfGeneric"].items, 1);
 
 table.insert(ProceduralDistributions.list["CrateCarpentry"].items, "Base.Pipe");
-table.insert(ProceduralDistributions.list["CrateCarpentry"].items, 8);
+table.insert(ProceduralDistributions.list["CrateCarpentry"].items, 12);
 
 table.insert(ProceduralDistributions.list["CrateFarming"].items, "Base.Pipe");
-table.insert(ProceduralDistributions.list["CrateFarming"].items, 5);
+table.insert(ProceduralDistributions.list["CrateFarming"].items, 12);
 
 table.insert(ProceduralDistributions.list["CrateMagazines"].items, "PotatoLauncher.POTATO_PotatoLauncherMag");
-table.insert(ProceduralDistributions.list["CrateMagazines"].items, 1);
+table.insert(ProceduralDistributions.list["CrateMagazines"].items, 3);
 
 table.insert(ProceduralDistributions.list["CrateMetalwork"].items, "Base.Pipe");
-table.insert(ProceduralDistributions.list["CrateMetalwork"].items, 1);
+table.insert(ProceduralDistributions.list["CrateMetalwork"].items, 8);
 
 table.insert(ProceduralDistributions.list["CrateOfficeSupplies"].items, "PotatoLauncher.POTATO_SprayPaint");
 table.insert(ProceduralDistributions.list["CrateOfficeSupplies"].items, 4);
@@ -64,26 +64,26 @@ table.insert(ProceduralDistributions.list["CrateRandomJunk"].items, "Base.Hairsp
 table.insert(ProceduralDistributions.list["CrateRandomJunk"].items, 0.8);
 
 table.insert(ProceduralDistributions.list["EngineerTools"].items, "PotatoLauncher.POTATO_PotatoLauncherMag");
-table.insert(ProceduralDistributions.list["EngineerTools"].items, 1);
+table.insert(ProceduralDistributions.list["EngineerTools"].items, 3);
 
 table.insert(ProceduralDistributions.list["FactoryLockers"].items, "PotatoLauncher.POTATO_SprayPaint");
 table.insert(ProceduralDistributions.list["FactoryLockers"].items, 1);
 table.insert(ProceduralDistributions.list["FactoryLockers"].items, "Base.Hairspray");
-table.insert(ProceduralDistributions.list["FactoryLockers"].items, 1);
+table.insert(ProceduralDistributions.list["FactoryLockers"].items, 4);
 table.insert(ProceduralDistributions.list["FactoryLockers"].items, "Base.WoodGlue");
 table.insert(ProceduralDistributions.list["FactoryLockers"].items, 1);
 
 table.insert(ProceduralDistributions.list["FireStorageTools"].items, "Base.Pipe");
-table.insert(ProceduralDistributions.list["FireStorageTools"].items, 5);
+table.insert(ProceduralDistributions.list["FireStorageTools"].items, 8);
 
 table.insert(ProceduralDistributions.list["GarageTools"].items, "Base.Pipe");
-table.insert(ProceduralDistributions.list["GarageTools"].items, 5);
+table.insert(ProceduralDistributions.list["GarageTools"].items, 8);
 
 table.insert(ProceduralDistributions.list["GardenStoreTools"].items, "Base.Pipe");
-table.insert(ProceduralDistributions.list["GardenStoreTools"].items, 2);
+table.insert(ProceduralDistributions.list["GardenStoreTools"].items, 5);
 
 table.insert(ProceduralDistributions.list["GasStorageMechanics"].items, "PotatoLauncher.POTATO_PotatoLauncherMag");
-table.insert(ProceduralDistributions.list["GasStorageMechanics"].items, 1);
+table.insert(ProceduralDistributions.list["GasStorageMechanics"].items, 3);
 
 table.insert(ProceduralDistributions.list["GigamartSchool"].items, "PotatoLauncher.POTATO_SprayPaint");
 table.insert(ProceduralDistributions.list["GigamartSchool"].items, 4);
@@ -92,19 +92,19 @@ table.insert(ProceduralDistributions.list["JanitorMisc"].items, "PotatoLauncher.
 table.insert(ProceduralDistributions.list["JanitorMisc"].items, 4);
 
 table.insert(ProceduralDistributions.list["LibraryBooks"].items, "PotatoLauncher.POTATO_PotatoLauncherMag");
-table.insert(ProceduralDistributions.list["LibraryBooks"].items, 1);
+table.insert(ProceduralDistributions.list["LibraryBooks"].items, 3);
 
 table.insert(ProceduralDistributions.list["LivingRoomShelf"].items, "PotatoLauncher.POTATO_PotatoLauncherMag");
-table.insert(ProceduralDistributions.list["LivingRoomShelf"].items, 0.2);
+table.insert(ProceduralDistributions.list["LivingRoomShelf"].items, 1);
 
 table.insert(ProceduralDistributions.list["LivingRoomShelfNoTapes"].items, "PotatoLauncher.POTATO_PotatoLauncherMag");
-table.insert(ProceduralDistributions.list["LivingRoomShelfNoTapes"].items, 0.2);
+table.insert(ProceduralDistributions.list["LivingRoomShelfNoTapes"].items, 1);
 
 table.insert(ProceduralDistributions.list["LockerArmyBedroom"].items, "PotatoLauncher.POTATO_SprayPaint");
 table.insert(ProceduralDistributions.list["LockerArmyBedroom"].items, 1);
 
 table.insert(ProceduralDistributions.list["MagazineRackMixed"].items, "PotatoLauncher.POTATO_PotatoLauncherMag");
-table.insert(ProceduralDistributions.list["MagazineRackMixed"].items, 0.8);
+table.insert(ProceduralDistributions.list["MagazineRackMixed"].items, 3);
 
 table.insert(ProceduralDistributions.list["OfficeShelfSupplies"].items, "PotatoLauncher.POTATO_SprayPaint");
 table.insert(ProceduralDistributions.list["OfficeShelfSupplies"].items, 4);
@@ -124,13 +124,13 @@ table.insert(ProceduralDistributions.list["PoliceLockers"].items, "Base.WoodGlue
 table.insert(ProceduralDistributions.list["PoliceLockers"].items, 2);
 
 table.insert(ProceduralDistributions.list["PostOfficeMagazines"].items, "PotatoLauncher.POTATO_PotatoLauncherMag");
-table.insert(ProceduralDistributions.list["PostOfficeMagazines"].items, 1);
+table.insert(ProceduralDistributions.list["PostOfficeMagazines"].items, 3);
 
 table.insert(ProceduralDistributions.list["RandomFiller"].items, "PotatoLauncher.POTATO_SprayPaint");
-table.insert(ProceduralDistributions.list["RandomFiller"].items, 2);
+table.insert(ProceduralDistributions.list["RandomFiller"].items, 4);
 
 table.insert(ProceduralDistributions.list["SchoolLockers"].items, "PotatoLauncher.POTATO_SprayPaint");
-table.insert(ProceduralDistributions.list["SchoolLockers"].items, 1);
+table.insert(ProceduralDistributions.list["SchoolLockers"].items, 3);
 
 table.insert(ProceduralDistributions.list["ToolStoreCarpentry"].items, "Base.Pipe");
 table.insert(ProceduralDistributions.list["ToolStoreCarpentry"].items, 10);
@@ -140,16 +140,16 @@ table.insert(ProceduralDistributions.list["ToolStoreMetalwork"].items, 6);
 
 
 table.insert(SuburbsDistributions["Bag_WeaponBag"].items, "PotatoLauncher.POTATO_PotatoLauncher");
-table.insert(SuburbsDistributions["Bag_WeaponBag"].items, 0.1);
+table.insert(SuburbsDistributions["Bag_WeaponBag"].items, 0.3);
 
 table.insert(SuburbsDistributions["RifleCase3"].items, "PotatoLauncher.POTATO_PotatoLauncher");
 table.insert(SuburbsDistributions["RifleCase3"].items, 1);
 
 table.insert(SuburbsDistributions["all"]["Outfit_Redneck"].items, "PotatoLauncher.POTATO_PotatoLauncher");
-table.insert(SuburbsDistributions["all"]["Outfit_Redneck"].items, 0.3);
+table.insert(SuburbsDistributions["all"]["Outfit_Redneck"].items, 0.7);
 
 table.insert(SuburbsDistributions["all"]["Outfit_Student"].items, "PotatoLauncher.POTATO_PotatoLauncher");
-table.insert(SuburbsDistributions["all"]["Outfit_Student"].items, 0.3);
+table.insert(SuburbsDistributions["all"]["Outfit_Student"].items, 0.8);
 
 table.insert(SuburbsDistributions["all"]["postbox"].items, "PotatoLauncher.POTATO_PotatoLauncherMag");
-table.insert(SuburbsDistributions["all"]["postbox"].items, 0.5);
+table.insert(SuburbsDistributions["all"]["postbox"].items, 1);
---
+++ Contents/mods/Potato Launcher/media/lua/shared/RecordedMedia/POTATO_recorded_media.lua
@@ -0,0 +1,29 @@
+RecMedia = RecMedia or {}
+
+-- Media: VHS: How to make a Potato Launcher [id: e5206ac2-3aee-11ed-a261-0242ac120002]
+RecMedia["e5206ac2-3aee-11ed-a261-0242ac120002"] = {
+	itemDisplayName = "IGUI_POTATO_vhs_name",
+	title = "IGUI_POTATO_vhs_title",
+	subtitle = "IGUI_POTATO_vhs_subtitle",
+	author = nil,
+	extra = nil,
+	spawning = 0,
+	category = "Retail-VHS",
+	lines = {
+		{ text = "IGUI_POTATO_vhs_1", r = 0.00, g = 0.69, b = 0.94, codes = "BOR-1" },
+		{ text = "IGUI_POTATO_vhs_2", r = 0.00, g = 0.69, b = 0.94, codes = "BOR-1" },
+		{ text = "IGUI_POTATO_vhs_3", r = 0.44, g = 0.19, b = 0.94, codes = "BOR-1" },
+		{ text = "IGUI_POTATO_vhs_4", r = 0.44, g = 0.19, b = 0.94, codes = "BOR-1" },
+		{ text = "IGUI_POTATO_vhs_5", r = 0.44, g = 0.19, b = 0.94, codes = "TRA+1" },
+		{ text = "IGUI_POTATO_vhs_6", r = 0.44, g = 0.19, b = 0.94, codes = "RCP=Make Potato Launcher" },
+		{ text = "IGUI_POTATO_vhs_7", r = 0.44, g = 0.19, b = 0.94, codes = "RCP=Make Metal Potato Launcher" },
+		{ text = "IGUI_POTATO_vhs_8", r = 0.44, g = 0.19, b = 0.94, codes = "TRA+1" },
+		{ text = "IGUI_POTATO_vhs_9", r = 0.44, g = 0.19, b = 0.94, codes = "RCP=Make Potato Launcher Fuel" },
+		{ text = "IGUI_POTATO_vhs_10", r = 0.44, g = 0.19, b = 0.94, codes = "TRA+1" },
+		{ text = "IGUI_POTATO_vhs_11", r = 0.44, g = 0.19, b = 0.94, codes = "BOR-1" },
+		{ text = "IGUI_POTATO_vhs_12", r = 0.44, g = 0.19, b = 0.94, codes = "BOR-1" },
+		{ text = "IGUI_POTATO_vhs_13", r = 0.44, g = 0.19, b = 0.94, codes = "BOR-1" },
+		{ text = "IGUI_POTATO_vhs_14", r = 0.44, g = 0.19, b = 0.94, codes = "BOR-1" },
+		{ text = "IGUI_POTATO_vhs_15", r = 0.44, g = 0.19, b = 0.94, codes = "BOR-1" },
+	},
+};
